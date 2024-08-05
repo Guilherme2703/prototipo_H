c141a2473fe149fea8b867c47ab7f2c046e2a488
Guilherme2703 <glimadefreitasgit2002@gmail.com>
2024-08-05 19:39:19 -0300

    Estilizando o menu de navegação

--- includes/header.php
+++ includes/header.php
@@ -31,16 +31,13 @@
                   <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link px-0" aria-current="page" href="#">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
+                                <a class="nav-link px-0 mx-3" href="#">Features</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <a class="nav-link px-0" href="#">Pricing</a>
                             </li>
                         </ul>
                   </div>
