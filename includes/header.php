6e68ccdb2979b8c0b2b9bdeb4c93d68b7b1826a0
Guilherme2703 <glimadefreitasgit2002@gmail.com>
2024-08-04 21:44:00 -0300

    criando header

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="src/style.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row justify-content-center justify-content-md-between align-items-center gap-3 gap-md-0 py-2">
+                <a href="/home.php" class="col-6"><img src="img/logo_hender.png" alt="Logo da hender" width="100px"></a>
+                <div class="col-12 col-md-4 col-xl-3">
+                    <form class="search-area rounder-3 p-3 d-flex flex-nowrap">
+                        <i class="bi bi-search me-2"></i>
+                        <input type="text" placeholder="Pesquisar produtos...">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
+    
