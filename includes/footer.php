756a5f54811a3f6c907a03d799488317444b66dc
Guilherme2703 <glimadefreitasgit2002@gmail.com>
2024-08-07 20:07:50 -0300

    Montando o Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,62 @@
 
+    <footer class="container-fluid footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg">
+                    <a href="/home.php"><img src="img/logo_hender.png" alt="Logo da hender" width="100px"></a>
+                    <p class="mt-3">
+                        Avepark - Parque de Ciência e <br>
+                        Tecnologia, S.A. <br>
+                        4805-017 Guimarães
+                    </p>
+
+                    <div>
+                        <i class="bi bi-facebook fs-3 me-3"></i>
+                        <i class="bi bi-instagram fs-3 me-3"></i>
+                    </div>
+                </div>
+                <div class="col-lg offset-lg-2 d-flex flex-column">
+                    <h4>Website</h4>
+                    <a href="#">Home</a>
+                    <a href="#">Sobre nós</a>
+                    <a href="#">Loja</a>
+                    <a href="#">Contato</a>
+                    <a href="#">A minha conta</a>
+                </div>
+
+                <div class="col-lg d-flex flex-column">
+                    <h4>Website</h4>
+                    <a href="#">Home</a>
+                    <a href="#">Sobre nós</a>
+                    <a href="#">Loja</a>
+                    <a href="#">Contato</a>
+                    <a href="#">A minha conta</a>
+                </div>
+
+                <div class="col-lg d-flex flex-column">
+                    <h4>Website</h4>
+                    <a href="#">Home</a>
+                    <a href="#">Sobre nós</a>
+                    <a href="#">Loja</a>
+                    <a href="#">Contato</a>
+                    <a href="#">A minha conta</a>
+                </div>
+
+                <div class="col-lg d-flex flex-column">
+                    <h4>Website</h4>
+                    <a href="#">Home</a>
+                    <a href="#">Sobre nós</a>
+                    <a href="#">Loja</a>
+                    <a href="#">Contato</a>
+                    <a href="#">A minha conta</a>
+                </div>
+            </div>
+            <div>
+                <hr>
+                <p>₢ 2024, inflight IT</p>
+            </div>
+        </div>
+    </footer>
 
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
     </body>
