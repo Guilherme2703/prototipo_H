6c7c522f09329346d71ea77d612f2c2d9cfedb2f
Guilherme2703 <glimadefreitasgit2002@gmail.com>
2024-08-09 20:10:26 -0300

    Iniciando a Home

--- home.php
+++ home.php
@@ -1,5 +1,44 @@
 <?php require_once('includes/header.php'); ?>
 
-
+    <section>
+        <div id="carouselExampleCaptions" class="carousel slide carousel-home" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="img/lixadeira-ferro.jpg" class="d-block w-100 img-carousel" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2>Hender,<br>seu parceiro na construção!</h2>
+                  <a href="#">Ver mais</a>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="img/lixadeira-madeira.jpg" class="d-block w-100 img-carousel" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2>Ferramentas que constroem <br> seus sonhos!</h2>
+                  <a href="#">Ver mais</a>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="img/broca.jpg" class="d-block w-100 img-carousel" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2>Equipando suas ideias,<br>construindo seu futuro!</h2>
+                  <a href="#">Ver mais</a>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+    </section>
 
 <?php require_once('includes/footer.php'); ?>
