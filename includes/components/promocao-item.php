aa93fd217d3b45a47a987c9ef2db6ff98fe5b82e
unknown <glimadefreitasgit2002@gmail.com>
2024-08-25 16:24:49 -0300

    Últimos detalhes da página home finalizada

--- includes/components/promocao-item.php
+++ includes/components/promocao-item.php
@@ -1,5 +1,6 @@
-<div class="col produto-item">
+<div class="col promocao-item">
     <div class="card my-4" >
+      <span class="tag-promo"><img class="img-promo" src="/img/promocao.png" alt="Etiqueta de promoção"></span>
         <img src="/img/serra_tico_tico.webp" class="card-img-top" alt="disco de corte inox starrett" width="100%">
         <div class="card-body">
           <h5 class="card-title mb-0 mt-3" >Serra Tico Tico Makita</h5>
