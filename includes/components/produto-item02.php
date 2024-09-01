753cdeade19e268c09746c40a9ab5a2d77a73c67
unknown <glimadefreitasgit2002@gmail.com>
2024-09-01 15:20:19 -0300

    Montando a estrutura dos cards

---
+++ includes/components/produto-item02.php
@@ -0,0 +1,10 @@
+<div class="col produto-item02">
+    <div class="card my-4" >
+        <img src="/img/disco-de-corte-inox-45-starrett.jpg" class="card-img-top" alt="disco de corte inox starrett" width="100%">
+        <div class="card-body">
+          <h5 class="card-title mb-0 mt-3" >Disco de Corte</h5>
+          <p class="card-text mt-3 mb-4">R$2,10</p>
+          <a href="#" class="btn-card mt-2">Ver opções</a>
+        </div>
+    </div>
+</div>
