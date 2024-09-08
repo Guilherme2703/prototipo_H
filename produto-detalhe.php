84d4e6d5effa223301893a09985d485fcb936068
unknown <glimadefreitasgit2002@gmail.com>
2024-09-08 09:04:58 -0300

    Criando o Carousel da página de produtos

--- produto-detalhe.php
+++ produto-detalhe.php
@@ -6,10 +6,42 @@
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="home.php" class="text-primary"><i class="bi bi-house-door text-primary me-1"></i>Home</a></li>
                     <li class="breadcrumb-item active" aria-current="page"><a href="loja.php"><i class="bi bi-basket text-primary me-1"></i>Loja</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Disco</li>
+                    <li class="breadcrumb-item active" aria-current="page">Serra marmore Makita</li>
                 </ol>
             </nav>
         </div>
+
+        <!-- Detalhe Produto -->
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5 bloco-carousel">
+                        <div id="carouselExampleIndicators" class="carousel-detalhe carousel slide">
+                            <ol class="carousel-indicators">
+                              <p data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"><img src="img/serra-mrmore-110-mm-1200-watts-220v-m0400b-makita_1_1614174069.webp" alt="" class="d-block w-100"></p>
+                              <p data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"><img src="img/serra-marmore1.webp" class="d-block w-100" alt=""></p>
+                              <p data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"><img src="img/serra-marmore2.webp" class="d-block w-100" alt=""></p>
+                              <p data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"><img src="img/serra-marmore4.webp" class="d-block w-100" alt=""></p>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="img/serra-mrmore-110-mm-1200-watts-220v-m0400b-makita_1_1614174069.webp" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="img/serra-marmore1.webp" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="img/serra-marmore2.webp" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="img/serra-marmore4.webp" class="d-block w-100" alt="...">
+                                </div>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
     </main>
 
