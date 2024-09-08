2758629d1ec15ad980a8147699e676b81af34d66
unknown <glimadefreitasgit2002@gmail.com>
2024-09-08 08:19:40 -0300

    Iniciando a página do produto

---
+++ produto-detalhe.php
@@ -0,0 +1,16 @@
+<?php require_once('includes/header.php'); ?>
+
+    <main>
+        <div class="container mb-5 pt-4">
+            <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="home.php" class="text-primary"><i class="bi bi-house-door text-primary me-1"></i>Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page"><a href="loja.php"><i class="bi bi-basket text-primary me-1"></i>Loja</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Disco</li>
+                </ol>
+            </nav>
+        </div>
+
+    </main>
+
+<?php require_once('includes/footer.php'); ?>
